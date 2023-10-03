7afe16fad3879052fd7e0937f1192da876ecb7fd
NoboNobo <irieda@gmail.com>
2023-10-03 14:12:29 +0900

    fix for picossci-can

--- main.go
+++ main.go
@@ -18,6 +18,19 @@ import (
 )
 
 const (
+	LED1      machine.Pin = 25
+	LED2      machine.Pin = 14
+	LED3      machine.Pin = 15
+	SW1       machine.Pin = 24
+	SW2       machine.Pin = 23
+	SW3       machine.Pin = 22
+	CAN_INT   machine.Pin = 16
+	CAN_RESET machine.Pin = 17
+	CAN_SCK   machine.Pin = 18
+	CAN_TX    machine.Pin = 19
+	CAN_RX    machine.Pin = 20
+	CAN_CS    machine.Pin = 21
+
 	DEBUG         = false
 	Lock2Lock     = 540
 	HalfLock2Lock = Lock2Lock / 2
@@ -25,13 +38,9 @@ const (
 )
 
 var (
-	spi   = machine.SPI0
-	csPin = machine.GP28
-)
-
-var (
-	js *joystick.Joystick
-	ph *pid.PIDHandler
+	spi = machine.SPI0
+	js  *joystick.Joystick
+	ph  *pid.PIDHandler
 )
 
 func init() {
@@ -49,6 +58,21 @@ func init() {
 			{MinIn: -32767, MaxIn: 32767, MinOut: -32767, MaxOut: 32767},
 		},
 	}, ph.RxHandler, ph.SetupHandler, pid.Descriptor)
+	LED1.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	LED2.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	LED3.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	LED1.High()
+	LED2.High()
+	LED3.High()
+	SW1.Configure(machine.PinConfig{Mode: machine.PinInput})
+	SW2.Configure(machine.PinConfig{Mode: machine.PinInput})
+	SW3.Configure(machine.PinConfig{Mode: machine.PinInput})
+	CAN_INT.Configure(machine.PinConfig{Mode: machine.PinInput})
+	CAN_RESET.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	CAN_RESET.Low()
+	time.Sleep(10 * time.Millisecond)
+	CAN_RESET.High()
+	time.Sleep(10 * time.Millisecond)
 }
 
 var (
@@ -95,13 +119,14 @@ func absInt32(n int32) int32 {
 }
 
 func main() {
+	LED1.Low()
 	log.SetFlags(log.Lmicroseconds)
 	if err := spi.Configure(
 		machine.SPIConfig{
 			Frequency: 500000,
-			SCK:       machine.GP2,
-			SDO:       machine.GP3,
-			SDI:       machine.GP4,
+			SCK:       CAN_SCK,
+			SDO:       CAN_TX,
+			SDI:       CAN_RX,
 			Mode:      0,
 		},
 	); err != nil {
@@ -152,7 +177,7 @@ func main() {
 		}
 		log.Print(scanner.Err())
 	}()
-	can := mcp2515.New(spi, csPin)
+	can := mcp2515.New(spi, CAN_CS)
 	can.Configure()
 	if err := can.Begin(mcp2515.CAN500kBps, mcp2515.Clock8MHz); err != nil {
 		log.Fatal(err)
