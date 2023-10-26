548613085ba63e15bfbe763277a23c9f901337a6
NoboNobo <irieda@gmail.com>
2023-10-26 14:51:19 +0900

    add dummy-mode tags

--- pid/pid_handler.go
+++ pid/pid_handler.go
@@ -1,6 +1,7 @@
 package pid
 
 import (
+	"diy-ffb-wheel/logger"
 	"fmt"
 	"machine"
 	"machine/usb"
@@ -264,6 +265,7 @@ func (m *PIDHandler) FreeEffect(id uint8) {
 
 // SetEffect reportId == 0x01
 func (m *PIDHandler) SetEffect(b []byte) {
+	logger.Debugln("SetEffect:", b)
 	var v SetEffectOutputData
 	_ = v.UnmarshalBinary(b)
 	effect := m.effectStates[v.EffectBlockIndex]
@@ -277,6 +279,7 @@ func (m *PIDHandler) SetEffect(b []byte) {
 
 // SetEnvelope reportId == 0x02
 func (m *PIDHandler) SetEnvelope(b []byte) {
+	logger.Debugln("SetEnvelope:", b)
 	var v SetEnvelopeOutputData
 	_ = v.UnmarshalBinary(b)
 	effect := m.effectStates[v.EffectBlockIndex]
@@ -288,6 +291,7 @@ func (m *PIDHandler) SetEnvelope(b []byte) {
 
 // SetCondition reportId == 0x03
 func (m *PIDHandler) SetCondition(b []byte) {
+	logger.Debugln("SetCondition:", b)
 	var v SetConditionOutputData
 	_ = v.UnmarshalBinary(b)
 	axis := v.ParameterBlockOffset
@@ -307,6 +311,7 @@ func (m *PIDHandler) SetCondition(b []byte) {
 
 // SetPeriodic reportId == 0x04
 func (m *PIDHandler) SetPeriodic(b []byte) {
+	logger.Debugln("SetPeriodic:", b)
 	var v SetPeriodicOutputData
 	_ = v.UnmarshalBinary(b)
 	effect := m.effectStates[v.EffectBlockIndex]
@@ -318,6 +323,7 @@ func (m *PIDHandler) SetPeriodic(b []byte) {
 
 // SetConstantForce reportId == 0x05
 func (m *PIDHandler) SetConstantForce(b []byte) {
+	logger.Debugln("SetConstantForce:", b)
 	var v SetConstantForceOutputData
 	_ = v.UnmarshalBinary(b)
 	effect := m.effectStates[v.EffectBlockIndex]
@@ -326,6 +332,7 @@ func (m *PIDHandler) SetConstantForce(b []byte) {
 
 // SetRampForce reportId == 0x06
 func (m *PIDHandler) SetRampForce(b []byte) {
+	logger.Debugln("SetRampForce:", b)
 	var v SetRampForceOutputData
 	_ = v.UnmarshalBinary(b)
 	effect := m.effectStates[v.EffectBlockIndex]
@@ -335,6 +342,7 @@ func (m *PIDHandler) SetRampForce(b []byte) {
 
 // SetCustomForceData reportId == 0x07
 func (m *PIDHandler) SetCustomForceData(b []byte) {
+	logger.Debugln("SetCustomForceData:", b)
 	var v SetCustomForceDataOutputData
 	_ = v.UnmarshalBinary(b)
 	// TODO: implement
@@ -342,6 +350,7 @@ func (m *PIDHandler) SetCustomForceData(b []byte) {
 
 // SetDownloadForceSample reportId == 0x08
 func (m *PIDHandler) SetDownloadForceSample(b []byte) {
+	logger.Debugln("SetDownloadForceSample:", b)
 	var v SetDownloadForceSampleOutputData
 	_ = v.UnmarshalBinary(b)
 	// TODO: implement
@@ -349,6 +358,7 @@ func (m *PIDHandler) SetDownloadForceSample(b []byte) {
 
 // EffectOperation reportId == 0x0a
 func (m *PIDHandler) EffectOperation(b []byte) {
+	logger.Debugln("EffectOperation:", b)
 	var v EffectOperationOutputData
 	_ = v.UnmarshalBinary(b)
 	switch v.Operation {
@@ -371,6 +381,7 @@ func (m *PIDHandler) EffectOperation(b []byte) {
 
 // BlockFree reportId == 0x0b
 func (m *PIDHandler) BlockFree(b []byte) {
+	logger.Debugln("BlockFree:", b)
 	var v BlockFreeOutputData
 	_ = v.UnmarshalBinary(b)
 	if v.EffectBlockIndex == 0xff {
@@ -382,6 +393,7 @@ func (m *PIDHandler) BlockFree(b []byte) {
 
 // DeviceControl reportId == 0x0c
 func (m *PIDHandler) DeviceControl(b []byte) {
+	logger.Debugln("DeviceControl:", b)
 	var v DeviceControlOutputData
 	_ = v.UnmarshalBinary(b)
 	switch v.Control {
@@ -402,6 +414,7 @@ func (m *PIDHandler) DeviceControl(b []byte) {
 
 // DeviceGain reportId == 0x0d
 func (m *PIDHandler) DeviceGain(b []byte) {
+	logger.Debugln("DeviceGain:", b)
 	var v DeviceGainOutputData
 	_ = v.UnmarshalBinary(b)
 	m.gain = v.Gain
@@ -409,6 +422,7 @@ func (m *PIDHandler) DeviceGain(b []byte) {
 
 // SetCustomForce reportId == 0x0e
 func (m *PIDHandler) SetCustomForce(b []byte) {
+	logger.Debugln("SetCustomForce:", b)
 	var v SetCustomForceOutputData
 	_ = v.UnmarshalBinary(b)
 	// TODO: implement
